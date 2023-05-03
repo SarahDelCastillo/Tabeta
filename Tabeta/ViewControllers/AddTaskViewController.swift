31a952e1e4b052eb9284d4b091116a14438461c9
Sarah Del Castillo <sarah.del-castillo@outlook.com>
2023-05-03 18:25:55 +0200

    Allow a schedule to be saved

--- Tabeta/ViewControllers/AddTaskViewController.swift
+++ Tabeta/ViewControllers/AddTaskViewController.swift
@@ -13,6 +13,8 @@ class AddTaskViewController: UIViewController {
     private var taskNameInput = InputWithLabel()
     private var submitButton = UIButton(configuration: .bordered())
     private var titleLabel = UILabel()
+    private var timeLabel = UILabel()
+    private var datePicker = UIDatePicker()
     
     var taskManager: TabeTaskManager?
     
@@ -38,6 +40,16 @@ class AddTaskViewController: UIViewController {
             field.placeholder = "Feed the dog"
         }
         
+        //MARK: Date picker -
+        timeLabel.translatesAutoresizingMaskIntoConstraints = false
+        timeLabel.text = "Schedule"
+        timeLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        datePicker.timeZone = TimeZone.current
+        datePicker.preferredDatePickerStyle = .compact
+        datePicker.datePickerMode = .time
+        
         
         //MARK: Submit -
         submitButton.setTitle("Add task!", for: .normal)
@@ -48,6 +60,8 @@ class AddTaskViewController: UIViewController {
         //MARK: Constraints -
         view.addSubview(titleLabel)
         view.addSubview(taskNameInput)
+        view.addSubview(timeLabel)
+        view.addSubview(datePicker)
         view.addSubview(submitButton)
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
@@ -56,7 +70,14 @@ class AddTaskViewController: UIViewController {
             taskNameInput.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             taskNameInput.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
             
-            submitButton.topAnchor.constraint(equalTo: taskNameInput.bottomAnchor, constant: 30),
+            timeLabel.topAnchor.constraint(equalTo: taskNameInput.bottomAnchor, constant: 30),
+            timeLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 28),
+            timeLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            
+            datePicker.centerYAnchor.constraint(equalTo: timeLabel.centerYAnchor),
+            datePicker.trailingAnchor.constraint(equalTo: timeLabel.trailingAnchor),
+            
+            submitButton.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 30),
             submitButton.centerXAnchor.constraint(equalTo: taskNameInput.centerXAnchor)
         ])
     }
@@ -69,13 +90,21 @@ class AddTaskViewController: UIViewController {
         Task {
             do {
                 let taskName = taskNameInput.getTextField().text ?? "No name"
-                let task = TabeTask(done: false, name: taskName, notifTimes: [])
+                let date = extractDate()
+                
+                let task = TabeTask(done: false, name: taskName, notifTimes: [date])
                 try await taskManager.create(task: task)
                 dismiss(animated: true)
             } catch {
                 logger.error("Create task failed with error: \(error)")
             }
         }
+    }
+    
+    private func extractDate() -> Int {
+        let formatStyle = Date.FormatStyle().hour(.defaultDigits(amPM: .omitted))
+        let date = datePicker.date.formatted()
+        return Int(date)! // There is no way this date can't be cast to Int
     }
 }
 
